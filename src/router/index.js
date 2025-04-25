6d84008bcb284b84e7c6b260e1a0738f5a51a4f1
leehendrick <imhendrick16@gmail.com>
2025-04-24 21:33:09 -0400

    feat: create login route

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        title: "Login",
+      }
+    },
+  ]
+});
+
+export default router;
