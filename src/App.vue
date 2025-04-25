2f3cde212a813bcbdd3e36a8b594c49861ce7b12
leehendrick <imhendrick16@gmail.com>
2025-04-24 21:31:48 -0400

    feat: add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
-
+  import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="bg-gray-100 min-h-screen p-8">
-    <h1 class="text-4xl font-bold text-blue-600">
-      Tailwind CSS v4 funcionando!
-    </h1>
-  </div>
+  <RouterView></RouterView>
 </template>
 
 <style scoped></style>
