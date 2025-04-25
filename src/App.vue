8dd09c246cd319093ab6bd77ef4c48b6e7458e47
leehendrick <imhendrick16@gmail.com>
2025-04-24 20:50:30 -0400

    install fresh vue & tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-gray-100 min-h-screen p-8">
+    <h1 class="text-4xl font-bold text-blue-600">
+      Tailwind CSS v4 funcionando!
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
