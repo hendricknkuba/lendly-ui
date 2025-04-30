d954afc1a1e25b22728cf93c251cb193198a3fa6
hendricknkuba <imhendrick16@gmail.com>
2025-04-29 22:42:46 -0400

    config: register pinia

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import '../public/css/style.css'
 import App from './App.vue'
 import router from "./router";
+import { createPinia } from 'pinia';
 
 createApp(App)
   .use(router)
+  .use(createPinia())
   .mount('#app')
