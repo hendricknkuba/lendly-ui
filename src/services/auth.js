e2f2b83ed79d10f95d3996634fa16367f4a56c75
hendricknkuba <imhendrick16@gmail.com>
2025-04-29 22:43:26 -0400

    feat: add login

--- src/services/auth.js
+++ src/services/auth.js
@@ -10,3 +10,12 @@ export const register = async (userData) => {
     throw error.response.data.data.message || 'Erro ao registrar'
   }
 };
+
+export const login = async (userData) => {
+  try {
+    const response = await axios.post(`${API_URL}/auth/login`, userData)
+    return response.data
+  } catch (error) {
+    throw error.response.data.data.message || 'Erro ao fazer login'
+  }
+}
